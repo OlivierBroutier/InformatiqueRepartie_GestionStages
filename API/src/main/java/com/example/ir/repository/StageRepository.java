487a121e5936090a2708ccb46dd46ad072b99950
Olivier BROUTIER <olivier.broutier@axopen.com>
2021-12-19 17:28:28 +0100

    Suppression front JSP

--- API/src/main/java/com/example/ir/repository/StageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ir.repository;
-
-import com.example.ir.entity.Stage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StageRepository extends JpaRepository<Stage, Integer> {
-}
--- Client/src/main/webapp/home.jsp
+++
@@ -1,6 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-
-<h1>Stage BTS</h1>
-<br>
-<h3>Bienvenue sur la page de gestion des stages</h3>
-<hr>
