bcf181e9707458bf891de74731827bbb43ad035f
Olivier BROUTIER <olivier.broutier@axopen.com>
2022-01-05 16:59:38 +0100

    feat: Amélioration pré-sélection champs inscription / modif stage

--- webapp/src/app/inscription/inscription.component.ts
+++ webapp/src/app/inscription/inscription.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {EntrepriseService} from "../shared/service/entreprise.service";
-import {Entreprise} from "../model/entreprise.model";
-import {Etudiant} from "../model/etudiant.model";
-import {EtudiantService} from "../shared/service/etudiant.service";
-import {Professeur} from "../model/professeur.model";
-import {ProfesseurService} from "../shared/service/professeur.service";
-import {Stage} from "../model/stage.model";
-import {AuthentificationService} from "../shared/service/authentification.service";
-import {StageService} from "../shared/service/stage.service";
-import {SuccessService} from "../shared/service/success.service";
-import {Location} from "@angular/common";
-import {Router} from "@angular/router";
+import { EntrepriseService } from '../shared/service/entreprise.service';
+import { Entreprise } from '../model/entreprise.model';
+import { Etudiant } from '../model/etudiant.model';
+import { EtudiantService } from '../shared/service/etudiant.service';
+import { Professeur } from '../model/professeur.model';
+import { ProfesseurService } from '../shared/service/professeur.service';
+import { Stage } from '../model/stage.model';
+import { AuthentificationService } from '../shared/service/authentification.service';
+import { StageService } from '../shared/service/stage.service';
+import { SuccessService } from '../shared/service/success.service';
+import { Router } from '@angular/router';
 import { Mission } from '../model/mission.model';
 
 @Component({
@@ -23,20 +22,18 @@ export class InscriptionComponent implements OnInit {
     public entreprises: Entreprise[] = [];
     public etudiants: Etudiant[] = [];
     public professeurs: Professeur[] = [];
-    public stage : Stage = {};
-    public type_stage: undefined ;
-    public date_debut: string= '';
-    public date_fin : string= '';
-    public stage_resultat? : Stage;
+
+    public stage: Stage = { typeStage: 'aucun' };
+    public stage_resultat?: Stage;
 
     constructor(
-        private entrepriseService : EntrepriseService,
-        private etudiantService : EtudiantService,
-        private professeurService : ProfesseurService,
-        private authentificationService: AuthentificationService,
-        private stageService : StageService,
+        private readonly entrepriseService : EntrepriseService,
+        private readonly etudiantService : EtudiantService,
+        private readonly professeurService : ProfesseurService,
+        private readonly authentificationService: AuthentificationService,
+        private readonly stageService : StageService,
         private readonly successService: SuccessService,
-        private router: Router
+        private readonly router: Router
     ) { }
 
     async ngOnInit(): Promise<void> {
@@ -44,9 +41,21 @@ export class InscriptionComponent implements OnInit {
         this.etudiants = await this.etudiantService.findAllEtudiant();
         this.professeurs = await this.professeurService.findAllProfesseur();
 
-        this.stage.professeur = this.authentificationService.professeur;
-        this.stage.entreprise = window.history.state.entreprise;
-        this.stage.etudiant = window.history.state.stagiaire;
+        if (this.authentificationService.userIsEtudiant) {
+            this.stage.etudiant = this.authentificationService.etudiant;
+        }
+
+        if (this.authentificationService.userIsProfesseur) {
+            this.stage.professeur = this.authentificationService.professeur;
+        }
+
+        if (window.history.state.stagiaire) {
+            this.stage.etudiant = window.history.state.stagiaire;
+        }
+
+        if (window.history.state.entreprise) {
+            this.stage.entreprise = window.history.state.entreprise;
+        }
     }
 
     public get missions(): Mission[] {
