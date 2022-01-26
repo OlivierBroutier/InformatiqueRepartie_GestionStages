b471496575df4f8c56e47878aa53e0f2d737a849
Olivier BROUTIER <olivier.broutier@axopen.com>
2022-01-26 20:35:27 +0100

    feat: Ajout méthodes pour savoir si un message est lu / supprimé
    Par rapport à l'utilisateur connecté

--- webapp/src/app/messagerie/messagerie.component.ts
+++ webapp/src/app/messagerie/messagerie.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthentificationService} from "../shared/service/authentification.service";
-import {Message} from "../model/message.model";
-import {MDBModalService} from "angular-bootstrap-md";
-import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
-import {MessagePopupComponent} from "../message-popup/message-popup.component";
+import { AuthentificationService } from '../shared/service/authentification.service';
+import { Message } from '../model/message.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MessagePopupComponent } from '../message-popup/message-popup.component';
+import { MessageUtilisateurTypeEnum } from '../model/message-utilisateur-type.enum';
+import { MessageUtilisateur } from '../model/messageUtilisateur.model';
 
 @Component({
   selector: 'app-messagerie',
@@ -44,6 +45,43 @@ export class MessagerieComponent implements OnInit {
   public consult(message?: Message) {
       let popup =this.modalService.open(MessagePopupComponent);
       popup.componentInstance.message = message??{};
+  }
+
+  public isLu(message?: Message) {
+      return this.getMessageEtudiantFromAuthentification(message)?.lu;
+  }
+
+  public isSupprime(message?: Message) {
+      return this.getMessageEtudiantFromAuthentification(message)?.supprime;
+  }
+
+  private getMessageEtudiantFromAuthentification(message?: Message): MessageUtilisateur | undefined {
+      if (!message) {
+          return undefined;
+      }
+
+      if (this.userIsProfesseur) {
+          if (message.expediteur?.messageUtilisateurType === MessageUtilisateurTypeEnum.PROFESSEUR
+              && message.expediteur?.id === this.authentificationService.professeur?.id) {
+              return message.expediteur;
+          }
+          const destinataire = message?.destinataires?.filter(d => d.messageUtilisateurType === MessageUtilisateurTypeEnum.PROFESSEUR)
+              .find(d => d.id === this.authentificationService.professeur?.id);
+          if (destinataire) {
+              return destinataire;
+          }
+      } else {
+          if (message?.expediteur?.messageUtilisateurType === MessageUtilisateurTypeEnum.ETUDIANT
+              && message?.expediteur?.id === this.authentificationService.etudiant?.id) {
+              return message.expediteur
+          }
+          const destinataire = message?.destinataires?.filter(d => d.messageUtilisateurType === MessageUtilisateurTypeEnum.ETUDIANT)
+              .find(d => d.id === this.authentificationService.etudiant?.id);
+          if (destinataire) {
+              return destinataire;
+          }
+      }
+      return undefined;
   }
 
 }
--- webapp/src/app/model/message-utilisateur-type.enum.ts
+++ webapp/src/app/model/message-utilisateur-type.enum.ts
@@ -1,4 +1,4 @@
 export enum MessageUtilisateurTypeEnum {
-    ETUDIANT,
-    PROFESSEUR
+    ETUDIANT= 'ETUDIANT',
+    PROFESSEUR = 'PROFESSEUR'
 }
