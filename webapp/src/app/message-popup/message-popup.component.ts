27b0b35ddddb479a86392f02860b364e0c831884
HUA Julie <juliehua11@yahoo.com>
2022-01-26 09:47:19 +0100

    problème_modal

--- webapp/src/app/message-popup/message-popup.component.ts
+++ webapp/src/app/message-popup/message-popup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Message} from "../model/message.model";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-message-popup',
@@ -9,9 +10,13 @@ import {Message} from "../model/message.model";
 export class MessagePopupComponent implements OnInit {
 
     public message : Message={};
-  constructor() { }
+  constructor(private modal: NgbActiveModal) { }
 
   ngOnInit(): void {
   }
 
+    public ferme() {
+      this.modal.close();
+
+    }
 }
